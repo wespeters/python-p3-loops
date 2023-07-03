24b393500e7edc48fb0b8efc6e20b3dc45325a46
Wesley Peters <wesleypeters@gmail.com>
2023-07-03 11:19:51 -0600

    write functions with for and while loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num**2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        num += 1
